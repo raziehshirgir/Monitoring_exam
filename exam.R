222a2d5f73ef6f832680801e3694a6fc60542241
raziehshirgir <114756919+raziehshirgir@users.noreply.github.com>
2023-02-16 10:08:47 +0100

    add RGB plot

--- exam.R
+++ exam.R
@@ -1,3 +1,4 @@
+
 # Import the required packages
 library(ncdf4)
 library(sp)
@@ -9,19 +10,24 @@ library(ggplot2)
 library(patchwork)
 
 # Set the working directory directory
-setwd("/Users/alireza/Desktop/data/tif_files/")
-# uploading the file
-fire21032014_tif <- raster("c_gls_BA300_QL_201503310000_GLOBE_PROBAV_V1.0.1.tiff")
-fire21032014_tif
-#prepearing data 
-fire21032014_tif_stack <- stack(fire21032014_tif)
-#ploting data
-plot(fire21032014_tif)
+setwd("C:/Users/amir/OneDrive - Alma Mater Studiorum Università di Bologna (1)/Rosa's dataset/tif_files/four_cases") 
 
-fire21032014_df <- as.data.frame(fire21032014_tif, xy = TRUE)%>%na.omit()  
+# Let's read the first dataset 
+# list all the files with pattern "gls"
+rlist <- list.files(pattern="gls")
+rlist
 
+# import all the files that has been listed before
+import <- lapply(rlist,raster)
+import
 
-# ggplot + raster 
+#stack them and plot them
+TGr <- stack(import)
+names(TGr) <- c("March 2017","March 2018", "March 2019", "March 2020", "March 2021")
 
-ggplot(fire21032014_df)   + 
-  geom_raster((fire21032014_df) , mapping = aes(x=x, y=y, fill=layer))  
+plot(TGr)
+# Let's change their names
+
+plot(TGr, names.attr=c("March 2017","March 2018", "March 2019", "March 2020", "March 2021"))
+
+plotRGB(TGr,  r=1, g=2, b=5, stretch = 'lin')
